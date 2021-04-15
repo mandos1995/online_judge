1df28f4872c996361c642f550b031ee79131cde5
mandos1995 <mandos19950620@gmail.com>
2021-04-15 17:50:19 +0900

    BOJ 4153 solved

---
+++ BOJ/전체문제/4153_직각삼각형.py
@@ -0,0 +1,12 @@
+while True:
+    a, b, c = map(int, input().split())
+    if a == 0 and b == 0 and c == 0:
+        break
+    if c ** 2 == a ** 2 + b ** 2:
+        print('right')
+    elif a ** 2 == b ** 2 + c ** 2:
+        print('right')
+    elif b ** 2 == a ** 2 + c ** 2:
+        print('right')
+    else:
+        print('wrong')
