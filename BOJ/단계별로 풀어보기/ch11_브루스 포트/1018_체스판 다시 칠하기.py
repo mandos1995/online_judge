e70e878fb1c6e449fec53dc2259dd550c21fb1b1
mandos1995 <mandos19950620@gmail.com>
2021-04-16 16:28:04 +0900

    BOJ 1018 solved

---
+++ BOJ/단계별로 풀어보기/ch11_브루스 포트/1018_체스판 다시 칠하기.py
@@ -0,0 +1,38 @@
+n, m = map(int, input().split())
+board = []
+count = []
+for _ in range(n):
+    board.append(list(input()))
+start_B = [['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+           ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+           ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+           ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+           ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+           ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+           ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+           ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B']]
+
+start_W = [['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+           ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+           ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+           ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+           ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+           ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+           ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+           ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W']]
+result = 64
+
+for i in range(n - 7):
+    for j in range(m - 7):
+        count1 = 0
+        count2 = 0
+
+        for k in range(8):
+            for l in range(8):
+                if board[i + k][j + l] != start_B[k][l]:
+                    count1 += 1
+                if board[i + k][j + l] != start_W[k][l]:
+                    count2 += 1
+
+        result = min(result, count1, count2)
+print(result)
