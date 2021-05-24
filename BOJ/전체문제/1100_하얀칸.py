46388e476a7c951581ff67a62c69cf1e03936c70
mandos1995 <mandos19950620@gmail.com>
2021-05-24 09:37:05 +0900

    BOJ 1100 sol

--- BOJ/전체문제/1100_하얀칸.py
+++ BOJ/전체문제/1100_하얀칸.py
@@ -8,5 +8,12 @@ board = [['W','F','W','F','W','F','W','F'],
          ['F','W','F','W','F','W','F','W'],]
 
 chess = []
-chess.append(input())
-print(chess)
+count = 0
+for _ in range(8):
+    chess.append(input())
+
+for i in range(8):
+    for j in range(8):
+        if board[i][j] == 'W' and chess[i][j] == 'F':
+            count += 1
+print(count)
